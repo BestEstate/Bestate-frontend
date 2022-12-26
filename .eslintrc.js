87073c664af867025826af5c2aa4d1b8816eec2a
Sunghyun627 <kksscc4@gmail.com>
2022-12-26 10:24:15 +0900

    chore: ESLint, Prettier 설정

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: '2022',
+    sourceType: 'module',
+  },
+  extends: [
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:import/typescript',
+    'plugin:jsx-a11y/recommended',
+    'prettier',
+    'plugin:prettier/recommended',
+  ],
+};
