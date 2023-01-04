e69bd7b9f315c43b05ef2b3a8039dd9a79db8e36
Sunghyun627 <kksscc4@gmail.com>
2023-01-04 14:28:43 +0900

    fix: icon 추가 및 이름 수정

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,3 +1,4 @@
 export { default as AvatarXsmall } from './icons/avatar-xsmall.svg';
-export { default as Dropdown } from './icons/dropdown.svg';
+export { default as Dropdown1 } from './icons/dropdown1.svg';
+export { default as Dropdown2 } from './icons/dropdown2.svg';
 export { default as Search } from './icons/search.svg';
