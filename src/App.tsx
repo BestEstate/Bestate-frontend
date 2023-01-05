83d329918163882f0974a29de35bc9ea62810543
Sunghyun627 <kksscc4@gmail.com>
2023-01-05 13:48:39 +0900

    style: footer 마크업 구현

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import { ThemeProvider } from 'styled-components';
 import { GlobalStyle } from './styles/globalStyles';
 import { theme } from './styles/theme';
 import Header from './components/Header';
+import Footer from './components/Footer';
 
 const App = () => {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyle />
       <Header />
+      <Footer />
     </ThemeProvider>
   );
 };
--- src/styles/globalStyles.ts
+++ src/styles/globalStyles.ts
@@ -36,6 +36,9 @@ export const GlobalStyle = createGlobalStyle`
     }
   }
   
+  a {
+    text-decoration: none;
+  }
   
 
   @font-face {
