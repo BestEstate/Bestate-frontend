730cbc2d530eeb268db486ae54ab3f29499cbef3
Sunghyun627 <kksscc4@gmail.com>
2023-01-06 14:45:21 +0900

    style: banner 초기 마크업 구현

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -4,3 +4,5 @@ export { default as Dropdown2 } from './icons/dropdown2.svg';
 export { default as Search } from './icons/search.svg';
 export { default as Hamburger } from './icons/hamburger.svg';
 export { default as Apartment } from './icons/apartment.svg';
+
+export { default as House } from './images/house.png';
