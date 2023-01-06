67a062c02c40527ba0e4f13a576e4f56ba7e787e
Sunghyun627 <kksscc4@gmail.com>
2023-01-06 14:43:56 +0900

    style: global Color 간략화 및 image asset type 구체적으로 설정

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -2,18 +2,13 @@ import { DefaultTheme } from 'styled-components';
 
 const color = {
   white: '#ffffff',
-  gray0: '#f9f8fa',
   gray1: '#e9ecef',
   gray2: '#dee2e6',
   gray3: '#ced4da',
-  gray4: '#adb5bd',
-  gray5: '#868e96',
-  gray6: '#495057',
-  gray7: '#343a40',
-  gray8: '#212529',
-  grap9: '#1B1B1B',
   black: '#000000',
-  green1: '#2f735a',
+  primary1: '#2f735a',
+  primary2: '#235644',
+  primary3: '#eaf1ef',
 };
 
 const font = {
--- src/types/assets.d.ts
+++ src/types/assets.d.ts
@@ -1,8 +1,24 @@
-declare module '*.ico';
-declare module '*.gif';
-declare module '*.png';
-declare module '*.jpg';
-declare module '*.jpeg';
-declare module '*.svg';
-declare module '*.ttf';
-declare module '*.woff';
+declare module '*.ico' {
+  const content: string;
+  export default content;
+}
+declare module '*.gif' {
+  const content: string;
+  export default content;
+}
+declare module '*.png' {
+  const content: string;
+  export default content;
+}
+declare module '*.jpg' {
+  const content: string;
+  export default content;
+}
+declare module '*.jpeg' {
+  const content: string;
+  export default content;
+}
+declare module '*.svg' {
+  const content: string;
+  export default content;
+}
